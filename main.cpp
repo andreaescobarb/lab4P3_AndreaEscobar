7469cb4976f674551f19467ec335f7d04ece1b91
dennisalejandro <dennisalenjandro@unitec.edu>
2017-11-03 16:20:41 -0600

    ShowChar

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 int Menu(vector<string>);
 int* show(int**, int, int);
+int* showChar(char**, int, int);
 int main (){
 	initscr();
 	vector<string> MenuItems;
@@ -100,5 +101,59 @@ int* show(int** Tabla, int Xlength, int Ylength) {
 	XY[1] = y;
 	return XY;
 }
+int* showChar(char** Tabla, int Xlength, int Ylength) {
+	//Se le ingresa la matriz, y las longitudes de la tabla
+	//Devuelve un arreglo, la posicion [0] es la pos. X, la posicion [1] es el Y
+	int* XY = new int[2];	
+	char ch = '.';
+	int x = 0;
+	int y = 0;
+	while (ch !='\n') {	
+		clear();
+		for (int i = 0;i<Ylength;i++) {
+			for (int j = 0;j<Xlength;j++) {
+				string p = "";
+				p+= Tabla[i][j];
+				addstr(p.c_str());
+			}
+			addstr("\n");
+		}
+		addstr(("x: "+to_string(x)+"\n").c_str());
+		addstr(("y: "+to_string(y)+"\n").c_str());
+		ch = getch();
+		//Key Down == 66
+		if (ch == 66) {
+			y++;
+			if (y >= Ylength) {
+				y = 0;
+			}
+		}
+		//Key UP == 65
+		if (ch == 65) {
+			y--;
+			if (y < 0) {
+				y = 7;
+			}
+		}
+		//Key Left == 68
+		if (ch == 68) {
+			x--;
+			if (x < 0) {
+				x = 7;
+			}
+		}
+		//Key Right == 67
+		if (ch == 67) {
+			x++;
+			if (x >= Xlength) {
+				x = 0;
+			}
+		}
+	}
+	XY[0] = x;
+	XY[1] = y;
+	return XY;
+}
+
 
 
